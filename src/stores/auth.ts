5db12fdb4609562b227750a7988192bd0198df06
plumeink <hansion@plumeink.com>
2023-09-06 11:31:24 +0800

    feat: add obfuscate tools

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,10 +1,10 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
-import { useLocalStorage } from '@vueuse/core'
-import { encode, decode } from 'msgpack-lite'
+import {ref} from 'vue'
+import {defineStore} from 'pinia'
+import {useLocalStorage} from '@vueuse/core'
+import {read, write} from '@/tools/obfuscate'
 
 export const useAuthStore = defineStore('auth', () => {
-    const permissions = useLocalStorage('permissions', {
+    const permissions = useLocalStorage(write('permissions'), {
         LOGIN: 'LOGIN',
         STATS: 'STATS',
         // 任务管理
@@ -22,73 +22,14 @@ export const useAuthStore = defineStore('auth', () => {
         // 用户管理
         MANAGER: 'MANAGER',
         USER_MANAGER: 'USER_MANAGER',
-    },{
-        mergeDefaults:true,
+    }, {
+        mergeDefaults: true,
         serializer: {
-            //TODO 2进制转16进制 + 转换位数调整 + 数组拆分与拼接
-            read: (v: any) => v ? () => {
-                console.log(123321)
-                function deobfuscate(uint8Array) {
-                    const decodedData = decode(uint8Array);
-
-                    const lastByte = new Uint8Array(1);
-                    lastByte[0] = 0;
-
-                    for (let i = 0; i < decodedData.length; i++) {
-                        const curByte = decodedData[i];
-                        decodedData[i] ^= lastByte[0];
-                        lastByte[0] = curByte;
-                    }
-
-                    return decodedData;
-                }
-                console.log(123321)
-
-                return decode(deobfuscate(new Uint8Array(decode(Buffer.from(v.split('/').map(item => parseInt(item, 16).toString()).join(','), 'utf-8')))))
-            } : null,
-            write: (v: any) => {
-                function obfuscate(uint8Array) {
-                    const lastByte = new Uint8Array(1);
-                    lastByte[0] = 0;
-
-                    for (let i = 0; i < uint8Array.length; i++) {
-                        const curByte = uint8Array[i];
-                        uint8Array[i] ^= lastByte[0];
-                        lastByte[0] = curByte;
-                    }
-
-                    return uint8Array;
-                }
-                function deobfuscate(uint8Array) {
-                    const decodedData = uint8Array;
-
-                    const lastByte = new Uint8Array(1);
-                    lastByte[0] = 0;
-
-                    for (let i = 0; i < decodedData.length; i++) {
-                        const curByte = decodedData[i];
-                        decodedData[i] ^= lastByte[0];
-                        lastByte[0] = curByte;
-                    }
-
-                    return decodedData;
-                }
-                console.log(123333321)
-                console.log('ob',new Uint8Array(encode(v)))
-                const decoder = new TextDecoder('utf-8');
-                const o = obfuscate(new Uint8Array(encode(v))).map(item => parseInt(item).toString(16)).join('/')
-                console.log('000', new Uint8Array(encode(v)))
-                console.log('111', obfuscate(new Uint8Array(encode(v))))
-                console.log('222' , deobfuscate(obfuscate(new Uint8Array(encode(v)))))
-                console.log('obb', o)
-                const encoder = new TextEncoder('utf-8');
-                console.log('deob',deobfuscate(o.split('/').map(item => parseInt(item, 16))))
-                console.log('deobb',decode(deobfuscate(o.split('/').map(item => parseInt(item, 16)))))
-
-                return obfuscate(new Uint8Array(encode(v))).split(',').map(item => parseInt(item).toString(16)).join('/')
-            },
+            //TODO 分隔符混淆
+            read: (v: any) => v ? read(v) : null,
+            write: (v: any) => write(v),
         },
     })
 
-    return { permissions }
+    return {permissions}
 })
---
+++ src/tools/obfuscate.ts
@@ -0,0 +1,86 @@
+import {encode, decode} from 'msgpack-lite'
+
+function obfuscate(bytes) {
+    let lastByte = 0;
+    const result = new Uint8Array(bytes.length);
+
+    for (let i = 0; i < bytes.length; i++) {
+        let curByte = bytes[i];
+        let tmpByte = curByte;
+        curByte ^= lastByte;
+        lastByte = tmpByte;
+        result[i] = curByte;
+    }
+
+    // 反转数组
+    result.reverse();
+
+    // 再次执行混淆操作
+    lastByte = 0;
+    for (let i = 0; i < result.length; i++) {
+        let curByte = result[i];
+        let tmpByte = curByte;
+        curByte ^= lastByte;
+        lastByte = tmpByte;
+        result[i] = curByte;
+    }
+
+    // 转换为二进制字符串，确保每个字节有 8 位
+    const binaryString = result.reduce((acc, curByte) => {
+        const byteBinaryString = curByte.toString(2).padStart(8, '0');
+        return acc + byteBinaryString;
+    }, '');
+
+    // 将二进制字符串转换回 Uint8Array
+    const resultBytes = new Uint8Array(binaryString.length / 8);
+    for (let i = 0; i < binaryString.length; i += 8) {
+        const byteBinaryString = binaryString.substr(i, 8);
+        const byteValue = parseInt(byteBinaryString, 2);
+        resultBytes[i / 8] = byteValue;
+    }
+
+    return resultBytes;
+}
+
+function deobfuscate(bytes) {
+    let lastByte = 0;
+    const result = new Uint8Array(bytes.length);
+
+    for (let i = 0; i < bytes.length; i++) {
+        let curByte = bytes[i];
+        curByte ^= lastByte;
+        lastByte = curByte;
+        result[i] = curByte;
+    }
+
+    // 反转数组
+    result.reverse();
+
+    // 再次执行反混淆操作
+    lastByte = 0;
+    for (let i = 0; i < result.length; i++) {
+        let curByte = result[i];
+        curByte ^= lastByte;
+        lastByte = curByte;
+        result[i] = curByte;
+    }
+
+    // 转换为二进制字符串，确保每个字节有 8 位
+    const binaryString = result.reduce((acc, curByte) => {
+        const byteBinaryString = curByte.toString(2).padStart(8, '0');
+        return acc + byteBinaryString;
+    }, '');
+
+    // 将二进制字符串转换回 Uint8Array
+    const resultBytes = new Uint8Array(binaryString.length / 8);
+    for (let i = 0; i < binaryString.length; i += 8) {
+        const byteBinaryString = binaryString.substr(i, 8);
+        const byteValue = parseInt(byteBinaryString, 2);
+        resultBytes[i / 8] = byteValue;
+    }
+
+    return resultBytes;
+}
+
+export const read = (v) => decode(deobfuscate(v.split('/').map(item => parseInt(item, 32))))
+export const write = (v) => Array.from(obfuscate(new Uint8Array(encode(v)))).map(item => item.toString(32)).join('/')
